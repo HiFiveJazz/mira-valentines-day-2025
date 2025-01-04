836e8ea2613bbe5faa861fcd158a5ea1a07158f1
HiFiveJazz <jasmeet.bhatia.us@gmail.com>
2025-01-03 22:58:49 -0600

    add anti-tilting on scroll

--- src/components/SizeableBox/SizeableBox.jsx
+++ src/components/SizeableBox/SizeableBox.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import VanillaTilt from 'vanilla-tilt';
 import './SizeableBox.css';
 
@@ -12,8 +12,10 @@ const SizeableBox = ({
   width = '300px',
 }) => {
   const cardRef = useRef(null);
+  const [isScrolling, setIsScrolling] = useState(false);
   let startX = 0; // Track initial touch X position
   let startY = 0; // Track initial touch Y position
+  let scrollTimeout;
 
   useEffect(() => {
     const currentCard = cardRef.current;
@@ -26,14 +28,25 @@ const SizeableBox = ({
       'max-glare': 0.5,
     });
 
+    // Detect scrolling
+    const handleScroll = () => {
+      setIsScrolling(true);
+      clearTimeout(scrollTimeout);
+      scrollTimeout = setTimeout(() => setIsScrolling(false), 500); // Add 0.25s delay after scrolling stops
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
     // Add touch event listeners for mobile
     const handleTouchStart = (event) => {
+      if (isScrolling) return; // Disable tilt during scroll
       const touch = event.touches[0];
       startX = touch.clientX;
       startY = touch.clientY;
     };
 
     const handleTouchMove = (event) => {
+      if (isScrolling) return; // Disable tilt during scroll
       const touch = event.touches[0];
       const rect = currentCard.getBoundingClientRect();
       const x = ((touch.clientX - startX) / rect.width) * 100;
@@ -43,6 +56,7 @@ const SizeableBox = ({
     };
 
     const handleTouchEnd = () => {
+      if (isScrolling) return; // Disable tilt during scroll
       // Reset card to original position after touch ends
       currentCard.style.transform = '';
     };
@@ -53,6 +67,7 @@ const SizeableBox = ({
 
     // Cleanup listeners
     return () => {
+      window.removeEventListener('scroll', handleScroll);
       currentCard.removeEventListener('touchstart', handleTouchStart);
       currentCard.removeEventListener('touchmove', handleTouchMove);
       currentCard.removeEventListener('touchend', handleTouchEnd);
@@ -61,7 +76,7 @@ const SizeableBox = ({
         currentCard.vanillaTilt.destroy();
       }
     };
-  }, []);
+  }, [isScrolling]);
 
   return (
     <div
