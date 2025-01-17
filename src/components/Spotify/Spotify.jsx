05cd9d74b93fe6a9907ec35276e688139476b4e4
HiFiveJazz <jasmeet.bhatia.us@gmail.com>
2025-01-16 18:00:04 -0800

    updated spotify css

--- src/components/Spotify/Spotify.jsx
+++ src/components/Spotify/Spotify.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Spotify = ({ playlistId, width = '100%', height = '352' }) => {
   return (
-    <div style={{ borderRadius: '12px', overflow: 'hidden', maxWidth: '100%' }}>
+
+    <div style={{ borderRadius: '12px', overflow: 'hidden', maxWidth: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', marginLeft: '4rem', marginRight: '4rem',}}>
       <iframe
-        style={{ borderRadius: '12px' }}
+        style={{ borderRadius: '12px',}}
         src={`https://open.spotify.com/embed/playlist/${playlistId}?utm_source=generator`}
         width={width}
         height={height}
