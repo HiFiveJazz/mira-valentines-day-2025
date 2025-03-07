fc2fdc9973d43c757ebb4aef045a3d786a404649
HiFiveJazz <jasmeet.bhatia.us@gmail.com>
2025-03-07 15:57:34 -0800

    update

--- src/components/MomentBox/imageImports.js
+++ src/components/MomentBox/imageImports.js
@@ -2,7 +2,13 @@ const imageList = [
   'moments-compressed/DSC_7662.webp',
   'moments-compressed/DSC_7661.webp',
   'moments-compressed/DSC_7650.webp',
-  'moments-compressed/DSC_7647.webp'
+  'moments-compressed/DSC_7647.webp',
+  'moments-compressed/DSC_7569.webp',
+  'moments-compressed/DSC_7519.webp',
+  'moments-compressed/DSC_7509.webp',
+  'moments-compressed/DSC_7463.webp',
+  'moments-compressed/DSC_7233.webp',
+  'moments-compressed/DSC_7202.webp'
 ];
 
 export default imageList;
